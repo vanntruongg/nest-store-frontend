267deecab72a82e899b18e71766023ddc85dc691
Van Truong <truongtran.lv2019@gmail.com>
2024-03-24 15:09:14 +0700

    update: remove next-auth

--- src/components/auth/facebook-button.tsx
+++ src/components/auth/facebook-button.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 import { FacebookLogo } from "./facebook-logo";
 
 const FacebookButton = () => {
   return (
     <button
       className="flex w-full justify-center items-center gap-5 rounded bg-white py-4 lg:px-4 text-sm font-bold drop-shadow-md hover:bg-gray-50"
-      onClick={() => signIn("facebook")}
+      // onClick={() => signIn("facebook")}
     >
       <FacebookLogo />
       <span className="text-sm lg:text-base">Tiếp tục với Facebook</span>
--- src/components/auth/google-button.tsx
+++ src/components/auth/google-button.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { GoogleLogo } from "./google-logo";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 
 const GoogleButton = () => {
   return (
     <button
       className="flex w-full justify-center items-center gap-5 rounded bg-white py-4 lg:px-4 text-sm font-bold drop-shadow-md hover:bg-gray-50"
-      onClick={() => signIn("google")}
+      // onClick={() => signIn("google")}
     >
       <GoogleLogo />
       <span className="text-sm lg:text-base">Tiếp tục với Google</span>
