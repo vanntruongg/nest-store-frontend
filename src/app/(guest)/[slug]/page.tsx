56c123144c43ade1c91e45b8062fb7a6ec6b9235
Van Truong <truongtran.lv2019@gmail.com>
2024-03-25 00:13:29 +0700

    add breadcrumb

--- src/app/(guest)/[slug]/page.tsx
+++ src/app/(guest)/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import productApi from "~/apis/produc-api";
+import { Category, Product } from "~/common/model/product.model";
 import { ProductUtil } from "~/common/utility/product.util";
+import Breadrumbs from "~/components/breadrumbs";
+import DevelopingTooltip from "~/components/developing-tooltip";
 import MaxWidthWrapper from "~/components/max-width-wrapper";
 import ProductDetail from "~/components/product/product-detail";
 import ProductReel from "~/components/product/product-reel";
@@ -24,23 +27,63 @@ interface PageProps {
 // ];
 
 const ProductDetailPage = async ({ params }: PageProps) => {
-  const result = await productApi.getProductById(
+  const result: any = await productApi.getProductById(
     ProductUtil.extractProductIdFromSlug(params.slug)
   );
   // console.log(result);
-  const category = result.payload.data.category.name;
-  const productName = result.payload.data.name;
+  const product: Product = result.payload.data.product;
+  const categories: Category[] = result.payload.data.categories;
+
+  const breadcrumbs: Breadrumb[] = [];
+  categories.map((category) => {
+    breadcrumbs.unshift({
+      id: category.id,
+      name: category.name,
+      href: ProductUtil.createSlugCategory(category.name, category.id),
+    });
+  });
 
   return (
-    <div className="bg-white">
-      <ProductDetail product={result.payload.data} />
+    <div className="bg-gray-100 flex flex-col gap-4">
+      <Breadrumbs
+        breadrumbs={breadcrumbs}
+        options={product.name}
+        optionPage={true}
+      />
+      <ProductDetail product={product} />
 
-      <div className=""></div>
+      <MaxWidthWrapper className="">
+        <div className="bg-white p-4 rounded-sm">
+          <div className="bg-gray-100">
+            <h2 className="text-xl p-2">Chi tiết sản phẩm</h2>
+          </div>
+          <div className="py-8 flex flex-col gap-2 text-sm">
+            <div className="flex gap-2 items-center">
+              <span className="text-muted-foreground">Danh mục:</span>
+              <Breadrumbs breadrumbs={breadcrumbs} className="h-4" />
+            </div>
+            <div className="flex gap-2">
+              <span className="text-muted-foreground">Chất liệu:</span>
+              <p>{product.material}</p>
+            </div>
+            <div className="flex gap-2">
+              <span className="text-muted-foreground">Phong cách:</span>
+              <p>{product.style}</p>
+            </div>
+          </div>
+          <div className="bg-gray-100">
+            <DevelopingTooltip>
+              <h2 className="text-xl p-2">Đánh giá sản phẩm</h2>
+            </DevelopingTooltip>
+          </div>
+          <div className=""></div>
+        </div>
+      </MaxWidthWrapper>
       <MaxWidthWrapper>
         <ProductReel
           href="/products"
           title={`Sản phẩm tương tự`}
-          subtitle={`Các sản phẩm thuộc ${category} tương tự như '${productName}'`}
+          subtitle={`Các sản phẩm thuộc ${product.category.name} tương tự như '${product.name}'`}
         />
       </MaxWidthWrapper>
     </div>
