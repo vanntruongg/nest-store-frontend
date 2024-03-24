577653823853ad2460fce409c1e60663912326a1
Van Truong <truongtran.lv2019@gmail.com>
2024-03-25 00:11:19 +0700

    update: add className prop

--- src/components/developing-tooltip.tsx
+++ src/components/developing-tooltip.tsx
@@ -5,17 +5,22 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
+import { cn } from "~/lib/utils";
 
 export interface DevelopingTooltipProps {
   children: ReactNode;
-  className: string;
+  className?: string;
 }
 
 const DevelopingTooltip = ({ children, className }: DevelopingTooltipProps) => {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={300}>
-        <TooltipTrigger className={className}>{children}</TooltipTrigger>
+        <TooltipTrigger
+          className={cn("text-muted-foreground opacity-50", className)}
+        >
+          {children}
+        </TooltipTrigger>
         <TooltipContent className="p-1 text-xs select-none">
           Chức năng đang phát triển
         </TooltipContent>
