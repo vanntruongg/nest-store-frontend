595b526250523ba2012d8c1ac2df6c02c5f85932
Van Truong <truongtran.lv2019@gmail.com>
2024-03-25 23:26:59 +0700

    feat: add filter by category

--- src/components/product/list-category.tsx
+++ src/components/product/list-category.tsx
@@ -1,4 +1,4 @@
-import { Category } from "~/common/model/product.model";
+import { ICategory } from "~/common/model/product.model";
 import {
   Accordion,
   AccordionContent,
@@ -9,48 +9,82 @@ import { buttonVariants } from "../ui/button";
 import Link from "next/link";
 import { cn } from "~/lib/utils";
 import { ProductUtil } from "~/common/utility/product.util";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 interface ListCategoryProps {
-  categories: Category[];
+  categories: ICategory[];
   fontSize: number;
+  fetchData: (categoryId: number, order: string, page: number) => Promise<void>;
 }
 
-const ListCategory = ({ categories, fontSize }: ListCategoryProps) => {
+const ListCategory = ({
+  categories,
+  fontSize,
+  fetchData,
+}: ListCategoryProps) => {
   fontSize = fontSize - 1;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
+  const handleFetchData = (categoryId: number) => {
+    fetchData(categoryId, "", 0);
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("category", categoryId.toString());
+    router.push(pathname + "?" + params.toString());
+  };
   return (
     <div className="flex flex-col gap-2 items-start">
-      {categories.map(({ id, name, children }, idx) =>
-        children ? (
-          <Accordion key={id} type="single" collapsible className="w-full">
+      {categories.map(({ category, subCategories }, idx) =>
+        subCategories && subCategories.length > 0 ? (
+          <Accordion
+            key={category.id}
+            type="single"
+            collapsible
+            className="w-full"
+          >
             <AccordionItem value="item-1" className="border-none">
               <AccordionTrigger
                 style={{ fontSize: fontSize }}
                 className="p-2 font-semibold text-gray-700"
               >
-                <Link href={`/${ProductUtil.createSlugCategory(name, idx)}`}>
-                  {name}
-                </Link>
+                <div
+                  // href={`/${ProductUtil.createSlugCategory(
+                  //   category.name,
+                  //   category.id
+                  // )}`}
+                  onClick={() => handleFetchData(category.id)}
+                >
+                  {category.name}
+                </div>
               </AccordionTrigger>
               <AccordionContent className="px-2">
-                <ListCategory categories={children} fontSize={fontSize} />
+                <ListCategory
+                  categories={subCategories}
+                  fontSize={fontSize}
+                  fetchData={fetchData}
+                />
               </AccordionContent>
             </AccordionItem>
           </Accordion>
         ) : (
-          <Link
-            key={id}
-            href={`/${ProductUtil.createSlugCategory(name, idx)}`}
+          <div
+            key={category.id}
+            // href={`/${ProductUtil.createSlugCategory(
+            //   category.name,
+            //   category.id
+            // )}`}
+            onClick={() => handleFetchData(category.id)}
             style={{ fontSize: fontSize }}
             className={cn(
               buttonVariants({
                 variant: "link",
-                className: "px-2 py-0 text-gray-700",
+                className: "px-2 py-0 text-gray-700 cursor-pointer",
               })
             )}
           >
-            {name}
-          </Link>
+            {category.name}
+          </div>
         )
       )}
     </div>
