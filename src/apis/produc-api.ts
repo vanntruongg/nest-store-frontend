3287ad873e3cd4423fd3f67941ab1f20e6925bd5
Van Truong <truongtran.lv2019@gmail.com>
2024-03-25 23:24:19 +0700

    update: add param category for method getList, add method getCategory

--- src/apis/produc-api.ts
+++ src/apis/produc-api.ts
@@ -1,12 +1,18 @@
 import httpClient from "~/common/http-client";
 
 const productApi = {
-  getList: (order: string, page: number) =>
-    httpClient.get<any>(`/product/products?order=${order}&page=${page}`),
+  getList: (category: number, order: string, page: number) =>
+    httpClient.get<any>(
+      `/product/products?category=${category}&order=${order}&page=${page}`
+    ),
   getProductById: (productId: number) =>
     httpClient.get<any>(`/product/get/${productId}`),
   getProductByName: (name: string) =>
     httpClient.get<any>(`/product/get/name?name=${name}`),
+  getAllSubCategory: (categoryId: string) =>
+    httpClient.get<any>(
+      `/product/category/subcategory/all-level/${categoryId}`
+    ),
 };
 
 export default productApi;
