3e2bf6936463152725f4ed2a77634e69246573dc
Van Truong <truongtran.lv2019@gmail.com>
2024-03-31 00:02:09 +0700

    add model

---
+++ src/common/model/cart.model.ts
@@ -0,0 +1,31 @@
+interface IItem {
+  id: number;
+  quantity: number;
+  price: number;
+  name: string;
+  category: string;
+  imageUrl: string;
+}
+interface ICartItem {
+  email: string;
+  items: IItem[];
+  totalPrice: number;
+}
+interface IAddRequest {
+  email: string;
+  itemDto: {
+    id: number;
+    quantity: number;
+    price: number;
+    name: string;
+    category: string;
+    imageUrl: string;
+  };
+}
+interface IUpdateCartRequest {
+  email: string;
+  itemId: number;
+  quantity: number;
+}
+
+export type { ICartItem, IItem, IUpdateCartRequest, IAddRequest };
