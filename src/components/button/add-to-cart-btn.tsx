afdd86a0084b233085608b0f03f156205c19c31b
Van Truong <truongtran.lv2019@gmail.com>
2024-03-31 00:05:45 +0700

    update: add to cart call api

--- src/components/button/add-to-cart-btn.tsx
+++ src/components/button/add-to-cart-btn.tsx
@@ -1,41 +1,61 @@
 "use client";
 import { useState } from "react";
 import { Button } from "../ui/button";
-import { ShoppingCart } from "lucide-react";
+import { Loader2, ShoppingCart } from "lucide-react";
+import { BaseUtil } from "~/common/utility/base.util";
+import { useUser } from "~/hooks/useUser";
+import { Product } from "~/common/model/product.model";
+import cartApi from "~/apis/cart-api";
+import { IAddRequest } from "~/common/model/cart.model";
 
-const AddtoCartButton = () =>
-  // { product }: { product: Product }
-  {
-    // const { addItem } = useWishlist();
-    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+interface AddtoCartButtonProps {
+  product: Product;
+  quantity: number;
+}
 
-    // useEffect(() => {
-    //   const timeout = setTimeout(() => {
-    //     setIsSuccess(false);
-    //   }, 2000);
+const AddtoCartButton = ({ product, quantity }: AddtoCartButtonProps) => {
+  const { user } = useUser();
+  const [loading, setLoading] = useState<boolean>(false);
 
-    //   return () => clearTimeout(timeout);
-    // }, [isSuccess]);
+  const handlAddtoCart = async () => {
+    setLoading(true);
+    try {
+      const data: IAddRequest = {
+        email: user.email,
+        itemDto: {
+          id: product.id,
+          quantity,
+          price: product.price,
+          name: product.name,
+          category: product.category.name,
+          imageUrl: product.imageUrl,
+        },
+      };
 
-    return (
-      <Button
-        onClick={() => {
-          // addItem(product);
-          // setIsSuccess(true);
-        }}
-        size={"sm"}
-        className="w-full border border-primary bg-purple-200 text-primary text-xs uppercase hover:bg-purple-100 hover:shadow-sm rounded-none transition-all duration-300"
-      >
-        {isSuccess ? (
-          "Đã thêm!"
-        ) : (
-          <div className="flex items-center gap-1 font-semibold">
-            <ShoppingCart strokeWidth={1.5} className="size-5" />
-            Thêm vào giỏ hàng
-          </div>
-        )}
-      </Button>
-    );
+      await cartApi.add(data);
+    } catch (error) {
+      BaseUtil.handleErrorApi({ error });
+    } finally {
+      setLoading(false);
+    }
   };
+
+  return (
+    <Button
+      onClick={handlAddtoCart}
+      size={"sm"}
+      className="w-full border border-primary bg-purple-200 text-primary text-xs uppercase hover:bg-purple-100 hover:shadow-sm rounded-none transition-all duration-300"
+    >
+      {loading ? (
+        <Loader2 strokeWidth={1.5} className="animate-spin" />
+      ) : (
+        <div className="flex items-center gap-1 font-semibold">
+          <ShoppingCart strokeWidth={1.5} className="size-5" />
+          Thêm vào giỏ hàng
+        </div>
+      )}
+    </Button>
+  );
+};
 
 export default AddtoCartButton;
