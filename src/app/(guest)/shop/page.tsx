9fdde4985e734c1f7440ef7d5f209c7f0d373fa5
Van Truong <truongtran.lv2019@gmail.com>
2024-04-02 03:07:38 +0700

    update layout page

--- src/app/(guest)/shop/page.tsx
+++ src/app/(guest)/shop/page.tsx
@@ -1,7 +1,15 @@
 import MaxWidthWrapper from "~/components/max-width-wrapper";
 import Breadrumbs from "~/components/breadrumbs";
-import ProductListing from "~/components/product/product-listing";
+import ProductListing from "~/app/(guest)/shop/product-listing";
 import Category from "~/components/category";
+import { cn } from "~/lib/utils";
+import ListCategory from "~/app/(guest)/shop/list-category";
+import productApi from "~/apis/produc-api";
+import { ChevronRight } from "lucide-react";
+import {
+  Category as CategoryModel,
+  ICategory,
+} from "~/common/model/product.model";
 
 const BREADRUMBS = [
   {
@@ -12,13 +20,39 @@ const BREADRUMBS = [
 ];
 
 const ShopPage = async () => {
+  const result = await productApi.getCategory();
+  const data: CategoryModel[] = result.payload.data;
+  const categories: ICategory[] = [];
+  data.map((category) =>
+    categories.unshift({ category: category, subCategories: [] })
+  );
+
+  categories.sort(
+    (category1, category2) => category1.category.id - category2.category.id
+  );
+
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 mb-6">
       <Breadrumbs breadrumbs={BREADRUMBS} />
       <MaxWidthWrapper className="">
         <Category />
       </MaxWidthWrapper>
-      <ProductListing />
+      <MaxWidthWrapper>
+        <div className={cn("grid grid-cols-5 gap-6")}>
+          <div className={cn("bg-white col-span-1")}>
+            <div className="p-6 flex justify-center border-b w-full relative">
+              <div className="absolute top-1/2 -translate-y-1/2 -right-4 p-1 bg-white text-muted-foreground rounded-full shadow cursor-pointer">
+                <ChevronRight size={24} />
+              </div>
+              <p className="uppercase text-muted-foreground">Danh mục</p>
+            </div>
+            <ListCategory categories={categories} fontSize={17} />
+          </div>
+          <div className="bg-white col-span-4">
+            <ProductListing />
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </div>
   );
 };
--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -4,19 +4,20 @@ import { IItem } from "~/common/model/cart.model";
 
 type CheckoutState = {
   items: IItem[];
-  addItem: (item: IItem) => void;
+  addItem: (item: IItem, quantity: number) => void;
   removeItem: (itemId: number) => void;
   addItems: (items: IItem[]) => void;
   clearCheckout: () => void;
+  updateQuantityItemCheckOut: (itemId: number, quantity: number) => void;
 };
 
 export const useCheckout = create<CheckoutState>()(
   persist(
     (set) => ({
       items: [],
-      addItem: (item) =>
+      addItem: (item, quantity) =>
         set((state) => {
-          return { items: [...state.items, item] };
+          return { items: [...state.items] };
         }),
       removeItem: (itemId) =>
         set((state) => ({
@@ -26,6 +27,12 @@ export const useCheckout = create<CheckoutState>()(
         set((state) => {
           return { items: items };
         }),
+      updateQuantityItemCheckOut: (itemId: number, newQuantity: number) =>
+        set((state) => ({
+          items: state.items.map((item) =>
+            item.id === itemId ? { ...item, quantity: newQuantity } : item
+          ),
+        })),
       clearCheckout: () => set({ items: [] }),
     }),
     {
