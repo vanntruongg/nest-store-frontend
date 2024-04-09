026935b0f380cc70ecd085e6affd34290682d095
Van Truong <truongtran.lv2019@gmail.com>
2024-04-10 01:33:57 +0700

    update: add apis order

--- src/apis/order-api.ts
+++ src/apis/order-api.ts
@@ -1,13 +1,29 @@
 import httpClient from "~/common/http-client";
+import { IOrderRequest } from "~/common/model/order.model";
 import { EndpointUtil } from "~/common/utility/endpoint.util";
 
 const orderApi = {
+  createOrder: (orderRequest: IOrderRequest) =>
+    httpClient.post<any>(EndpointUtil.NEST.ORDER.CREATE_ORDER, orderRequest),
   getUrlPaymentVNPay: (amount: number) =>
     httpClient.get<any>(
       EndpointUtil.NEST.ORDER.GET_URL_PAYMENT_VNPAY + `?amount=${amount}`
     ),
   getAllPaymentMethod: () =>
     httpClient.get<any>(EndpointUtil.NEST.ORDER.GET_ALL_PAYMENT_METHOD),
+  getAllByEmail: (email: string) =>
+    httpClient.get<any>(
+      EndpointUtil.NEST.ORDER.GET_BY_EMAIL + `?email=${email}`
+    ),
+  getByStatus: (status: string) =>
+    httpClient.get<any>(
+      EndpointUtil.NEST.ORDER.GET_BY_STATUS + `?status=${status}`
+    ),
+  getByEmailAndStatus: (email: string, status: string) =>
+    httpClient.get<any>(
+      EndpointUtil.NEST.ORDER.GET_BY_EMAIL_AND_STATUS +
+        `?email=${email}&status=${status}`
+    ),
 };
 
 export default orderApi;
