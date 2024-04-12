73cafad1d0fe6aa1f8a3d383b1e408a4330361ce
Van Truong <truongtran.lv2019@gmail.com>
2024-04-13 02:12:52 +0700

    update: add method handle check phone number

--- src/common/utility/base.util.ts
+++ src/common/utility/base.util.ts
@@ -2,7 +2,6 @@ import { UseFormSetError } from "react-hook-form";
 import { toast } from "~/components/ui/use-toast";
 import { EntityError } from "../http-client";
 import { IOrderShippingDetail } from "../model/order.model";
-import { ERole } from "./enum.util";
 
 export class BaseUtil {
   static handleErrorApi({
@@ -31,4 +30,26 @@ export class BaseUtil {
   static isShippingDetailEmpty = (shippingDetail: IOrderShippingDetail) => {
     return !shippingDetail.phone || !shippingDetail.address;
   };
+
+  static validateVietnamesePhoneNumber(number: string): boolean {
+    const pattern: RegExp =
+      /^(0)(90|91|92|93|94|96|97|98|162|163|164|165|166|167|168|169|12[0-9]|18[0-9]|19[0-9]|38|36|35|34|33|32|31|39|56|58|59|52|51|50|55|70|76|77|78|79|81|82|83|84|85|86|88|89|99|94|15[6-9]|15[0-5]|52|56|58|59|50|59|71|72|73|75|76|77|78|79|8[1-9]|91|92|93|94|95|96|97|98|99)\d{7}$/;
+
+    // Match the pattern
+    return pattern.test(number);
+  }
+
+  static checkPhoneAndSetErrorForm(
+    phone: string,
+    setError: UseFormSetError<any>
+  ) {
+    if (phone !== "" && !BaseUtil.validateVietnamesePhoneNumber(phone)) {
+      setError("phone", {
+        type: "client",
+        message: "Số điện thoại không hợp lệ",
+      });
+      return false;
+    }
+    return true;
+  }
 }
