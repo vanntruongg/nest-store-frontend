4912cd05dff03eae78ddfb2a33ada5cf1d08a44d
Van Truong <truongtran.lv2019@gmail.com>
2024-04-13 02:11:48 +0700

    update: api user

--- src/apis/user-api.ts
+++ src/apis/user-api.ts
@@ -1,12 +1,19 @@
 import httpClient from "~/common/http-client";
+import { IUpdateUser } from "~/common/model/user.model";
+import { EndpointUtil } from "~/common/utility/endpoint.util";
 
 const userApi = {
   getProfile: (accessToken: string) =>
-    httpClient.get<any>("/auth/profile", {
+    httpClient.get<any>(EndpointUtil.NEST.USER.GET_PROFILE, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     }),
+  getAllUser: () => httpClient.get<any>(EndpointUtil.NEST.USER.GET_ALL),
+  updateUser: (data: IUpdateUser) =>
+    httpClient.post<any>(EndpointUtil.NEST.USER.UPDATE_USER, data),
+  deleteUser: (email: string) =>
+    httpClient.post<any>(EndpointUtil.NEST.USER.DELETE_USER + `/${email}`),
 };
 
 export default userApi;
