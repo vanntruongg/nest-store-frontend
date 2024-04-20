745a5b074d3fa619404b75ef4dab18858d92cf0f
Van Truong <truongtran.lv2019@gmail.com>
2024-04-20 16:46:16 +0700

    fix: use access token from parameter

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,10 @@
 import { jwtDecode } from "jwt-decode";
 import { IJWTDecoded } from "~/common/model/auth.model";
-import { tokenStorage } from "~/common/utility/auth.util";
 import { UserRole } from "~/common/utility/enum.util";
 
 export const useAuth = () => {
-  const tokenDecoded: IJWTDecoded = jwtDecode(
-    tokenStorage.value.rawToken.accessToken
-  );
-
-  const isAdmin = () => {
+  const isAdmin = (accessToken: string) => {
+    const tokenDecoded: IJWTDecoded = jwtDecode(accessToken);
     return tokenDecoded.roles.includes(UserRole.ADMIN);
   };
 
