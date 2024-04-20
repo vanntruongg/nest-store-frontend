9b36e2e0a2cbd2c1586d36d748edd9c835a9f474
Van Truong <truongtran.lv2019@gmail.com>
2024-04-20 16:46:46 +0700

    feat: complete change password

--- src/app/(guest)/user/password/page.tsx
+++ src/app/(guest)/user/password/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Eye, EyeOff } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 import { useForm } from "react-hook-form";
 import userApi from "~/apis/user-api";
 import {
@@ -14,7 +12,6 @@ import { BaseUtil } from "~/common/utility/base.util";
 import { Button } from "~/components/ui/button";
 import { toast } from "~/components/ui/use-toast";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import {
   Form,
   FormControl,
@@ -24,14 +21,7 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 
-interface ResetPassword {
-  oldPassword: boolean;
-  newPassword: boolean;
-  confirmPassword: boolean;
-  [key: string]: boolean;
-}
-
-const ResetPasswordPage = () => {
+const ChangePasswordPage = () => {
   const router = useRouter();
   const form = useForm<TChangePasswordShema>({
     resolver: zodResolver(ChangePasswordShema),
@@ -41,35 +31,17 @@ const ResetPasswordPage = () => {
       confirmPassword: "",
     },
   });
-  const [showPassword, setShowPassword] = useState<ResetPassword>({
-    oldPassword: false,
-    newPassword: false,
-    confirmPassword: false,
-  });
 
-  const [passwords, setPasswords] = useState<IChangePasswordRequest>({
-    oldPassword: "",
-    newPassword: "",
-    confirmPassword: "",
-  });
-
-  const handleToggle = (key: string) => {
-    setShowPassword((preValue) => ({
-      ...preValue,
-      [key]: !preValue[key],
-    }));
-  };
-
-  const onSubmit = async () => {
+  const onSubmit = async (data: IChangePasswordRequest) => {
     try {
       const result = await userApi.changePassword(
-        passwords.oldPassword,
-        passwords.newPassword
+        data.oldPassword,
+        data.newPassword
       );
       toast({ description: result.payload.message });
       router.push("/user/profile");
     } catch (error) {
-      BaseUtil.handleErrorApi({ error });
+      BaseUtil.handleErrorApi({ error, setError: form.setError });
     }
   };
 
@@ -82,45 +54,6 @@ const ResetPasswordPage = () => {
         </p>
       </div>
       <div className="max-w-2xl min-w-[600px] mx-auto">
-        {/* <form action="" className="flex flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <Label className="w-40">Mật khẩu cũ</Label>
-            <PasswordToggle
-              type="oldPassword"
-              password={passwords.oldPassword}
-              setPasswords={setPasswords}
-              showPassword={showPassword.oldPassword}
-              onClick={() => handleToggle("oldPassword")}
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <Label className="w-40">Mật khẩu mới</Label>
-            <PasswordToggle
-              type="newPassword"
-              password={passwords.newPassword}
-              setPasswords={setPasswords}
-              showPassword={showPassword.newPassword}
-              onClick={() => handleToggle("newPassword")}
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <Label className="w-40">Xác nhận mật khẩu</Label>
-            <PasswordToggle
-              type="confirmPassword"
-              password={passwords.confirmPassword}
-              setPasswords={setPasswords}
-              showPassword={showPassword.confirmPassword}
-              onClick={() => handleToggle("confirmPassword")}
-            />
-          </div>
-
-          <Button
-            className="ml-[138px] self-start"
-            onClick={handleChangePassword}
-          >
-            Lưu
-          </Button>
-        </form> */}
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -133,7 +66,7 @@ const ResetPasswordPage = () => {
                 <FormItem>
                   <FormLabel>Mật khẩu cũ:</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input type="password" {...field} />
                   </FormControl>
                   <FormMessage className="text-xs" />
                 </FormItem>
@@ -146,7 +79,7 @@ const ResetPasswordPage = () => {
                 <FormItem>
                   <FormLabel>Mật khẩu mới:</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input type="password" {...field} />
                   </FormControl>
                   <FormMessage className="text-xs" />
                 </FormItem>
@@ -159,12 +92,14 @@ const ResetPasswordPage = () => {
                 <FormItem>
                   <FormLabel>Xác nhận mật khẩu:</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input type="password" {...field} />
                   </FormControl>
                   <FormMessage className="text-xs" />
                 </FormItem>
               )}
             />
+
+            <Button onClick={form.handleSubmit(onSubmit)}>Lưu thay đổi</Button>
           </form>
         </Form>
       </div>
@@ -172,53 +107,4 @@ const ResetPasswordPage = () => {
   );
 };
 
-export default ResetPasswordPage;
-
-interface PasswordToggleProps {
-  password: string;
-  type: string;
-  setPasswords: Dispatch<SetStateAction<IChangePasswordRequest>>;
-  showPassword: boolean;
-  onClick: (show: boolean) => void;
-}
-
-const PasswordToggle = ({
-  password,
-  type,
-  setPasswords,
-  showPassword,
-  onClick,
-}: PasswordToggleProps) => {
-  const handlePasswordChange = (
-    event: ChangeEvent<HTMLInputElement>,
-    type: string
-  ) => {
-    setPasswords((prevPasswords: IChangePasswordRequest) => ({
-      ...prevPasswords,
-      [type]: event.target.value,
-    }));
-  };
-
-  return (
-    <div className="relative w-full">
-      <Input
-        type={showPassword ? "text" : "password"}
-        value={password}
-        onChange={(e) => handlePasswordChange(e, type)}
-      />
-      <div className="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer">
-        {showPassword ? (
-          <Eye
-            className="size-4 text-muted-foreground"
-            onClick={() => onClick(false)}
-          />
-        ) : (
-          <EyeOff
-            className="size-4 text-muted-foreground"
-            onClick={() => onClick(true)}
-          />
-        )}
-      </div>
-    </div>
-  );
-};
+export default ChangePasswordPage;
