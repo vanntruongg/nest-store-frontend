bb763534e4ad6cd2d4b59816e8ce1f8ef860f4fd
Van Truong <truongtran.lv2019@gmail.com>
2024-04-21 17:28:38 +0700

    update: add skeleton

--- src/app/(guest)/shop/list-category.tsx
+++ src/app/(guest)/shop/list-category.tsx
@@ -9,6 +9,7 @@ import {
 import { buttonVariants } from "../../../components/ui/button";
 import { cn } from "~/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Skeleton } from "~/components/ui/skeleton";
 
 interface ListCategoryProps {
   categories: ICategory[];
@@ -29,54 +30,68 @@ const ListCategory = ({ categories, fontSize }: ListCategoryProps) => {
 
   return (
     <div className="flex flex-col gap-2 items-start">
-      {categories.map(({ category, subCategories }) =>
-        subCategories && subCategories.length > 0 ? (
-          <Accordion
-            key={category.id}
-            type="single"
-            collapsible
-            className="w-full"
-          >
-            <AccordionItem value="item-1" className="border-none">
-              <AccordionTrigger
-                style={{ fontSize: fontSize }}
-                className="p-2 font-semibold text-gray-700 hover:no-underline hover:bg-gray-100"
-              >
-                <div
-                  onClick={() => handleFetchData(category.id)}
-                  className="hover:underline"
+      {categories.length > 0 ? (
+        categories.map(({ category, subCategories }) =>
+          subCategories && subCategories.length > 0 ? (
+            <Accordion
+              key={category.id}
+              type="single"
+              collapsible
+              className="w-full"
+            >
+              <AccordionItem value="item-1" className="border-none">
+                <AccordionTrigger
+                  style={{ fontSize: fontSize }}
+                  className="p-2 font-semibold text-gray-700 hover:no-underline hover:bg-gray-100"
                 >
-                  {category.name}
-                </div>
-              </AccordionTrigger>
-              <AccordionContent className="px-2">
-                <ListCategory
-                  categories={subCategories}
-                  fontSize={fontSize}
-                  // fetchData={fetchData}
-                />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        ) : (
-          <div
-            key={category.id}
-            onClick={() => handleFetchData(category.id)}
-            style={{ fontSize: fontSize }}
-            className={cn(
-              buttonVariants({
-                variant: "link",
-                className:
-                  "px-2 py-0 w-full flex justify-start text-gray-700 cursor-pointer",
-              })
-            )}
-          >
-            {category.name}
-          </div>
+                  <div
+                    onClick={() => handleFetchData(category.id)}
+                    className="hover:underline"
+                  >
+                    {category.name}
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="px-2">
+                  <ListCategory
+                    categories={subCategories}
+                    fontSize={fontSize}
+                    // fetchData={fetchData}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ) : (
+            <div
+              key={category.id}
+              onClick={() => handleFetchData(category.id)}
+              style={{ fontSize: fontSize }}
+              className={cn(
+                buttonVariants({
+                  variant: "link",
+                  className:
+                    "px-2 py-0 w-full flex justify-start text-gray-700 cursor-pointer",
+                })
+              )}
+            >
+              {category.name}
+            </div>
+          )
         )
+      ) : (
+        <Placeholder />
       )}
     </div>
   );
 };
 
 export default ListCategory;
+
+const Placeholder = () => {
+  return (
+    <div className="w-full p-2 space-y-4">
+      <Skeleton className=" w-full h-8 bg-gray-200" />
+      <Skeleton className=" w-full h-8 bg-gray-200" />
+      <Skeleton className=" w-full h-8 bg-gray-200" />
+    </div>
+  );
+};
--- src/components/product/product-detail.tsx
+++ src/components/product/product-detail.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import MaxWidthWrapper from "../max-width-wrapper";
 
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
@@ -9,108 +9,133 @@ import Image from "next/image";
 import AddtoWishlistIcon from "../button/add-to-wishlist-icon";
 import { ProductUtil } from "~/common/utility/product.util";
 import { Product } from "~/common/model/product.model";
-import cartApi from "~/apis/cart-api";
-import { useUser } from "~/hooks/useUser";
-import { BaseUtil } from "~/common/utility/base.util";
-import useDebounce from "~/hooks/useDebounce";
+import { Skeleton } from "../ui/skeleton";
 
 interface ProductDetailProps {
   product?: Product;
 }
 
 const ProductDetail = ({ product }: ProductDetailProps) => {
-  const { user } = useUser();
   const [quantity, setQuantity] = useState<number>(1);
 
   const inCreaseQuantity = () => {
     setQuantity((quantity) => quantity + 1);
-    // debounce();
   };
   const deCreaseQuantity = () => {
     setQuantity((quantity) => (quantity > 1 ? quantity - 1 : 1));
-    // debounce();
   };
-
-  // const debouncedUpdateQuantity = useDebounce(quantity, 1000);
-
-  // useEffect(() => {
-  //   updateQuantity();
-  // }, [quantity, debouncedUpdateQuantity]);
 
   return (
     <MaxWidthWrapper>
-      <div className="bg-white flex gap-8 p-10 rounded-sm">
-        <div className="w-full grid grid-cols-10 gap-2">
-          <div className="col-span-8 aspect-square bg-gray-100 relative">
-            <Image
-              fill
-              src={product ? product.imageUrl : "/assets/product-default.jpg"}
-              alt="image product"
-              className="object-center size-full"
-              sizes="100"
-              loading="lazy"
-            />
-          </div>
-        </div>
-        <div className="w-full flex flex-col">
-          <div className="flex justify-between gap-6">
-            <h1 className="text-3xl">{product?.name}</h1>
-            <AddtoWishlistIcon product={product} />
-          </div>
-          <section className="mt-6 space-y-6">
-            <p className="text-lg font-bold bg-gray-50 p-2">
-              {ProductUtil.formatPrice(product ? product.price : 0)}
-            </p>
-            <div className="flex items-center">
-              <Check
-                aria-hidden="true"
-                className="size-5 flex-shrink-0 text-green-500"
+      {product ? (
+        <div className="bg-white flex gap-8 p-10 rounded-sm">
+          <div className="w-full grid grid-cols-10 gap-2">
+            <div className="col-span-8 aspect-square bg-gray-100 relative">
+              <Image
+                fill
+                src={product ? product.imageUrl : "/assets/product-default.jpg"}
+                alt="image product"
+                className="object-center size-full"
+                sizes="100"
+                loading="lazy"
               />
-              <p className="ml-2 text-sm text-muted-foreground">
-                Miễn phí vận chuyển
+            </div>
+          </div>
+          <div className="w-full flex flex-col">
+            <div className="flex justify-between gap-6">
+              <h1 className="text-3xl">{product?.name}</h1>
+              <AddtoWishlistIcon product={product} />
+            </div>
+            <section className="mt-6 space-y-6">
+              <p className="text-lg font-bold bg-gray-50 p-2">
+                {ProductUtil.formatPrice(product ? product.price : 0)}
               </p>
-            </div>
-            <div className="flex items-center gap-8 py-2">
-              <span>Quantity</span>
-              <div className="flex border border-gray-300">
-                <div className="p-2 max-w-20 border-r flex justify-center">
-                  <input
-                    type="number"
-                    value={quantity}
-                    onChange={(e) => setQuantity(parseInt(e.target.value))}
-                    className="w-full font-light text-center"
-                  />
+              <div className="flex items-center">
+                <Check
+                  aria-hidden="true"
+                  className="size-5 flex-shrink-0 text-green-500"
+                />
+                <p className="ml-2 text-sm text-muted-foreground">
+                  Miễn phí vận chuyển
+                </p>
+              </div>
+              <div className="flex items-center gap-8 py-2">
+                <span>Quantity</span>
+                <div className="flex border border-gray-300">
+                  <div className="p-2 max-w-20 border-r flex justify-center">
+                    <input
+                      type="number"
+                      value={quantity}
+                      onChange={(e) => setQuantity(parseInt(e.target.value))}
+                      className="w-full font-light text-center"
+                    />
+                  </div>
+                  <div className="flex flex-col divide-y divide-gray-300">
+                    <button
+                      onClick={inCreaseQuantity}
+                      className="p-1 text-gray-500 hover:bg-black hover:text-white transition-all duration-300"
+                    >
+                      <ChevronUp strokeWidth={1.5} className="size-5" />
+                    </button>
+                    <button
+                      onClick={deCreaseQuantity}
+                      className="p-1 text-gray-500 hover:bg-black hover:text-white transition-all duration-300"
+                    >
+                      <ChevronDown strokeWidth={1.5} className="size-5" />
+                    </button>
+                  </div>
                 </div>
-                <div className="flex flex-col divide-y divide-gray-300">
-                  <button
-                    onClick={inCreaseQuantity}
-                    className="p-1 text-gray-500 hover:bg-black hover:text-white transition-all duration-300"
-                  >
-                    <ChevronUp strokeWidth={1.5} className="size-5" />
-                  </button>
-                  <button
-                    onClick={deCreaseQuantity}
-                    className="p-1 text-gray-500 hover:bg-black hover:text-white transition-all duration-300"
-                  >
-                    <ChevronDown strokeWidth={1.5} className="size-5" />
-                  </button>
+              </div>
+              <div className="text-sm">Còn lại: {product?.stock} sản phẩm</div>
+              <div className="flex gap-4">
+                <div className="flex-1">
+                  <AddtoCartButton product={product} quantity={quantity} />
+                </div>
+                <div className="flex-1">
+                  <BuyNowButton />
                 </div>
               </div>
-            </div>
-            <div className="text-sm">Còn lại: {product?.stock} sản phẩm</div>
-            <div className="flex gap-4">
-              <div className="flex-1">
-                <AddtoCartButton product={product} quantity={quantity} />
-              </div>
-              <div className="flex-1">
-                <BuyNowButton />
-              </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
-      </div>
+      ) : (
+        <Placeholder />
+      )}
     </MaxWidthWrapper>
   );
 };
 
 export default ProductDetail;
+
+const Placeholder = () => {
+  return (
+    <div className="bg-white flex gap-8 p-10 rounded-sm">
+      <div className="w-full grid grid-cols-10 gap-2">
+        <Skeleton className="col-span-8 aspect-square bg-gray-200 relative" />
+      </div>
+      <div className="w-full flex flex-col justify-between">
+        <div className="flex flex-col justify-between gap-2">
+          <Skeleton className="bg-gray-200 w-full h-6" />
+          <Skeleton className="bg-gray-200 w-2/3 h-6" />
+          <Skeleton className="bg-gray-200 w-1/3 h-6" />
+        </div>
+        <section className="mt-6 space-y-6">
+          <Skeleton className="bg-gray-200 w-2/3 h-6" />
+
+          <div className="flex items-center">
+            <Skeleton className="bg-gray-200 w-2/3 h-6" />
+          </div>
+          <div className="flex items-center gap-8 py-2">
+            <Skeleton className="bg-gray-200 w-2/3 h-10" />
+          </div>
+          <Skeleton className="bg-gray-200 w-1/3 h-6" />
+
+          <div className="flex gap-4">
+            <Skeleton className="bg-gray-200 w-full h-12" />
+            <Skeleton className="bg-gray-200 w-full h-12" />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
