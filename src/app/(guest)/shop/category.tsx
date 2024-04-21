0a03b11b0617666ac4f5cf6045a95e0f737bb826
Van Truong <truongtran.lv2019@gmail.com>
2024-04-21 17:29:23 +0700

    update: call api get top level category

--- src/app/(guest)/shop/category.tsx
+++ src/app/(guest)/shop/category.tsx
@@ -1,61 +1,87 @@
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import productApi from "~/apis/produc-api";
+import { Category as CategoryModel } from "~/common/model/product.model";
+import { BaseUtil } from "~/common/utility/base.util";
 import { ProductUtil } from "~/common/utility/product.util";
-
-const categories = [
-  {
-    id: 1,
-    name: "Thời trang nam",
-    image:
-      "https://img.freepik.com/free-photo/non-binary-person-modern-clothes-posing-outdoors_23-2148760572.jpg?w=826&t=st=1706722366~exp=1706722966~hmac=6c19f24825f1a1e36cf351f5579f96d9e2379da9ca866b62a1546d20cf4d631f",
-  },
-  {
-    id: 2,
-    name: "Thời trang nữ",
-    image:
-      "https://img.freepik.com/free-photo/city-life-young-beautiful-girl-warm-clothes-have-walk-city-her-weekends-time_146671-16847.jpg?size=626&ext=jpg&ga=GA1.1.969645129.1706716916&semt=ais",
-  },
-  {
-    id: 3,
-    name: "Nam và nữ",
-    image:
-      "https://img.freepik.com/free-photo/young-japanese-couple_23-2148870761.jpg?w=740&t=st=1706723919~exp=1706724519~hmac=252ef3b432512966716e1fbfb44af0ae374fd45e7dc89f6e3d037db8c8add26b",
-  },
-];
+import { Skeleton } from "~/components/ui/skeleton";
 
 const Category = () => {
+  const [categories, setCategories] = useState<CategoryModel[]>([]);
+  const router = useRouter();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await productApi.getTopLevelCategory();
+        setCategories(result.payload.data);
+      } catch (error) {
+        BaseUtil.handleErrorApi({ error });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleRedirect = (name: string, id: number) => {
+    localStorage.setItem("category", name);
+    router.push(ProductUtil.createSlugCategory(name, id));
+  };
   return (
     <div className="bg-white">
       <h3 className="p-4 text-base uppercase text-muted-foreground shadow">
         Danh mục
       </h3>
       <div className="flex justify-between items-center divide-x">
-        {categories.map(({ id, name, image }) => (
-          <Link
-            key={id}
-            href={ProductUtil.createSlugCategory(name, id)}
-            className="w-full text-center p-6 capitalize hover:shadow-sm relative group"
-          >
-            <div className="w-1/3 mx-auto flex flex-col gap-2">
-              <div className="aspect-square bg-gray-100 rounded-full overflow-hidden relative">
-                <Image
-                  fill
-                  sizes="full"
-                  src={image}
-                  alt={`image category ${name}`}
-                  className="bg-gray-50 group-hover:scale-110 transition duration-300"
-                  loading="lazy"
-                  placeholder="blur"
-                  blurDataURL={image}
-                />
+        {categories.length > 0 ? (
+          categories.map(({ id, name, image }) => (
+            <div
+              key={id}
+              onClick={() => handleRedirect(name, id)}
+              className="w-full text-center p-6 capitalize hover:shadow-sm cursor-pointer relative group"
+            >
+              <div className="w-1/3 mx-auto flex flex-col gap-2">
+                <div className="aspect-square bg-gray-100 rounded-full overflow-hidden relative">
+                  <Image
+                    fill
+                    sizes="full"
+                    src={image}
+                    alt={`image category ${name}`}
+                    className="bg-gray-50 group-hover:scale-110 transition duration-300"
+                    loading="lazy"
+                    placeholder="blur"
+                    blurDataURL={image}
+                  />
+                </div>
+                <p className="text-nowrap">{name}</p>
               </div>
-              <p className="text-nowrap">{name}</p>
             </div>
-          </Link>
-        ))}
+          ))
+        ) : (
+          <CategoryPlaholder />
+        )}
       </div>
     </div>
   );
 };
 
 export default Category;
+
+const CategoryPlaholder = () => {
+  return (
+    <div className="flex w-full justify-between items-center divide-x">
+      {Array.from({ length: 3 }, (_, i) => (
+        <div
+          key={i}
+          className="flex-1 text-center p-6 capitalize hover:shadow-sm relative group"
+        >
+          <div className="w-1/3 mx-auto flex flex-col gap-2">
+            <div className="aspect-square bg-gray-100 rounded-full overflow-hidden relative">
+              <Skeleton className="w-full h-full rounded-full bg-gray-200" />
+            </div>
+            <Skeleton className="text-nowrap w-full mx-auto h-2 bg-gray-200" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
