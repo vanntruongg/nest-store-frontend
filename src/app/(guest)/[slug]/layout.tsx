a00e211c1d338076a8b2c5f2f9d4dfff4cbeef9c
Van Truong <truongtran.lv2019@gmail.com>
2024-04-21 17:27:59 +0700

    update: generateMetadata

--- src/app/(guest)/[slug]/layout.tsx
+++ src/app/(guest)/[slug]/layout.tsx
@@ -1,18 +1,31 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
+import productApi from "~/apis/produc-api";
+import { Product } from "~/common/model/product.model";
+import { ProductUtil } from "~/common/utility/product.util";
+// export const metadata: Metadata = {
+//   title: "[Tên sản phẩm]",
+//   description:
+//     "Khám phá chi tiết sản phẩm [Tên sản phẩm] từ NEST Store. Hãy tìm hiểu thêm về đặc điểm, mô tả, và giá của sản phẩm để có quyết định mua hàng chính xác!",
+// };
 
-export const metadata: Metadata = {
-  title: "[Tên sản phẩm]",
-  description:
-    "Khám phá chi tiết sản phẩm [Tên sản phẩm] từ NEST Store. Hãy tìm hiểu thêm về đặc điểm, mô tả, và giá của sản phẩm để có quyết định mua hàng chính xác!",
-};
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+export async function generateMetadata({ params }: Props) {
+  const productId = ProductUtil.extractProductIdFromSlug(params.slug);
 
-// export async function generateMetadata({ params }) {
-//   return {
-//     title: '...',
-//   }
-// }
+  const result = await productApi.getProductById(productId);
+  const product: Product = result.payload.data.product;
 
-export default function ContactLayout({ children }: { children: ReactNode }) {
+  return {
+    title: product.name,
+    description: `Khám phá chi tiết sản phẩm ${product.name} từ NEST Store. Hãy tìm hiểu thêm về đặc điểm, mô tả, và giá của sản phẩm để có quyết định mua hàng chính xác!`,
+  };
+}
+
+export default function ShopLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
