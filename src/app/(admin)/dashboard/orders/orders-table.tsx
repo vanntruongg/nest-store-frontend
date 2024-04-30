cc94c97d03e4e2a4df8366c591f93daff66d95b9
Van Truong <truongtran.lv2019@gmail.com>
2024-04-30 11:32:03 +0700

    fix: pagination table

--- src/app/(admin)/dashboard/orders/orders-table.tsx
+++ src/app/(admin)/dashboard/orders/orders-table.tsx
@@ -332,28 +332,28 @@ export function OrdersTable() {
           </TableBody>
         </Table>
       </div>
-      {table.getRowModel().rows?.length > 5 && (
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Trước
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              Tiếp
-            </Button>
-          </div>
+      {/* {table.getRowModel().rows?.length > 5 && ( */}
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Trước
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Tiếp
+          </Button>
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 }
